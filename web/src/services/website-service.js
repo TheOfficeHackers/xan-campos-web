8198c1e78ad8285b6025cfd84a7b5ad02873565b
Lechuza7 <103664285+Lechuza7@users.noreply.github.com>
2023-05-26 17:56:26 +0200

    Update website-service.js

--- web/src/services/website-service.js
+++ web/src/services/website-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: "https://xan-campos-web-api.onrender.com",
   withCredentials: true,
 });
 
